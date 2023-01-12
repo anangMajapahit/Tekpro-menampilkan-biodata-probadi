c0d93d9d19869c94b66d911bf3330dc5265ff9f1
AnangMajapahit <86045046+anangMajapahit@users.noreply.github.com>
2023-01-12 12:42:37 +0700

    main

---
+++ MenampilkanBiodataPribadi.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menampilkan.biodata.pribadi;
+
+/**
+ *
+ * @author EVI
+ */
+public class MenampilkanBiodataPribadi {
+    public static void main(String[] args) {
+        String nama = "AlifaWijiPangestu";
+        String nim = "09040620047";
+        String Asal = "Kutorejo";
+        String asalSekolah = "SMAN1KUTOREJO";
+        String nomerTelpon = "085856462532";
+        String hobi = "hiking";
+        String alasanMasukSI = "Ingin Pintar";
+        String tanggalLahir = "17-04-2003";
+        float ipk = 3.50f;
+        int umur = 2021-2003;
+        double bb = 45;
+        double tb = 1.58;
+        double bmi = bb/(tb*tb);
+        
+        String nama1 = "Yuniar Dwi Permatasari";
+        String nim1 = "09040620048";
+        String Asal1 = "Dlanggu";
+        String asalSekolah1 = "SMAN1Dlanggu";
+        String nomerTelpon1 = "085856545679";
+        String hobi1 = "BelajarKimia";
+        String alasanMasukSI1 = "InginMenjadiProgremer";
+        String tanggalLahir1 = "01-06-2002";
+        float ipk1 = 3.60f;
+        int umur1 = 2021-2002;
+        double bb1 = 45;
+        double tb1 = 1.60;
+        double bmi1 = bb/(tb*tb);
+
+        String nama2 = "Orlyn";
+        String nim2 = "09040620049";
+        String Asal2 = "KalimantanBarat";
+        String asalSekolah2 = "SMAN1KalimantanBarat";
+        String nomerTelpon2 = "0858566787652";
+        String hobi2 = "MakeUp";
+        String alasanMasukSI2 = "InginSukses";
+        String tanggalLahir2 = "28-01-2000";
+        float ipk2 = 3.90f;
+        int umur2 = 2021-2000;
+        double bb2 = 65;
+        double tb2 = 1.68;
+        double bmi2 = bb/(tb*tb);
+        
+        
+        System.out.println("nama Teman 1 = "+ nama);
+        System.out.println("nim teman 1 = "+ nim);
+        System.out.println("asal sekolah teman 1 = "+ asalSekolah);
+        System.out.println("nomerTelepon teman 1 = "+ nomerTelpon);
+        System.out.println("hobi teman 1 ="+ hobi );
+        System.out.println("alasanMasukSI teman 1 ="+ alasanMasukSI);
+        System.out.println("tanggalLahir Teman 1 = "+ tanggalLahir);
+        System.out.println("ipk teman 1 = "+ ipk);
+        System.out.println("umur teman 1 = "+ umur);
+        System.out.println("berat badan teman 1 = "+ bb);
+        System.out.println("tinggi badan teman 1 = "+ tb);
+        System.out.println("bmi teman 1 = " + bmi);
+        
+        
+        System.out.println("---------------------"); 
+        System.out.println("nama Teman 2 = "+ nama1);
+        System.out.println("nim teman 2 = "+ nim1);
+        System.out.println("asal sekolah teman 2 = "+ asalSekolah1);
+        System.out.println("nomerTelepon teman 2 = "+ nomerTelpon1);
+        System.out.println("hobi teman 2 ="+ hobi1);
+        System.out.println("alasanMasukSI teman 2 ="+ alasanMasukSI1);
+        System.out.println("tanggalLahir Teman 2 = "+ tanggalLahir1);
+        System.out.println("ipk teman 2 = "+ ipk1);
+        System.out.println("umur teman 2 = "+ umur1);
+        System.out.println("berat badan teman 2 = "+ bb1);
+        System.out.println("tinggi badan teman 2 = "+ tb1);
+        System.out.println("bmi teman 2 = " + bmi1);
+        
+        
+        System.out.println("---------------------");
+        System.out.println("nama Teman 3 = "+ nama2);
+        System.out.println("nim teman 3 = "+ nim2);
+        System.out.println("asal sekolah teman 3 = "+ asalSekolah2);
+        System.out.println("nomerTelepon teman 3 = "+ nomerTelpon2);
+        System.out.println("hobi teman 3 ="+ hobi2);
+        System.out.println("alasanMasukSI teman 3 ="+ alasanMasukSI2);
+        System.out.println("tanggalLahir Teman 3 = "+ tanggalLahir2);
+        System.out.println("ipk teman 3 = "+ ipk2);
+        System.out.println("umur teman 3 = "+ umur2);
+        System.out.println("berat badan teman 3 = "+ bb2);
+        System.out.println("tinggi badan teman 3 = "+ tb2);
+        System.out.println("bmi teman 3 = " + bmi2);
+                
+                
+    }
+    
+}
